73e6e28afccf4ca8ba0921230ab53e49d7429078
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-05-05 19:11:11 +0900

    add: meta tags

--- components/home/home.tsx
+++ components/home/home.tsx
@@ -1,42 +1,69 @@
 import Head from 'next/head'
 
 import UI from './ui'
-import LinkButton from '../common/link-button'
+import LinkButton from './link-button'
 
-const Home = () => (
-  <div className="flex flex-col min-h-screen">
-    <Head>
-      <title>ShinyPoems | シャニマス衣装ポエム検索</title>
-      <link
-        rel="stylesheet"
-        href="http://olicons.yemaosheji.com/css/olicons.css"
-      />
-    </Head>
-    <header className="flex items-center justify-center w-full h-28 mb-2 bg-shiny">
-      <div className="text-white text-center">
-        <p className="text-4xl font-bold">ShinyPoems</p>
-        <p className="text-sm mt-1">シャニマス衣装ポエム検索</p>
-      </div>
-    </header>
-    <main className="flex-grow mx-4">
-      <UI />
-    </main>
-    <footer className="flex flex-col items-center justify-center w-full h-48 mt-10 p-3 bg-shiny text-white text-center">
-      <div>
-        <LinkButton href="https://twitter.com/arrow_2nd" icon="ol-twitter-f" />
-        <LinkButton
-          href="https://github.com/arrow2nd/shiny-poems"
-          icon="ol-github-f"
+type Props = {
+  title: string
+  subtitle: string
+  desc: string
+  url: string
+  ogpImg: string
+}
+
+const Home = (props: Props) => {
+  const title = `${props.title} | ${props.subtitle}`
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Head>
+        <title>{title}</title>
+        <meta />
+        <meta name="description" content={props.desc} />
+        <meta name="image" content="" />
+        <meta property="og:url" content={props.url} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={props.desc} />
+        <meta property="og:image" content={props.ogpImg} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="arrow_2nd" />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={props.desc} />
+        <meta name="twitter:image" content={props.ogpImg} />
+        <link
+          rel="stylesheet"
+          href="http://olicons.yemaosheji.com/css/olicons.css"
         />
-      </div>
-      <div className="mt-3">
-        <p>
-          The copyright of the contents related to THE IDOLM@STER belongs to
-          BANDAI NAMCO Entertainment Inc.
-        </p>
-      </div>
-    </footer>
-  </div>
-)
+      </Head>
+      <header className="flex items-center justify-center w-full h-28 mb-2 bg-shiny">
+        <div className="text-white text-center">
+          <p className="text-4xl font-bold">{props.title}</p>
+          <p className="text-sm mt-1">{props.subtitle}</p>
+        </div>
+      </header>
+      <main className="flex-grow mx-4">
+        <UI />
+      </main>
+      <footer className="flex flex-col items-center justify-center w-full h-48 mt-10 p-3 bg-shiny text-white text-center">
+        <div>
+          <LinkButton
+            href="https://twitter.com/arrow_2nd"
+            icon="ol-twitter-f"
+          />
+          <LinkButton
+            href="https://github.com/arrow2nd/shiny-poems"
+            icon="ol-github-f"
+          />
+        </div>
+        <div className="mt-3">
+          <p>
+            The copyright of the contents related to THE IDOLM@STER belongs to
+            BANDAI NAMCO Entertainment Inc.
+          </p>
+        </div>
+      </footer>
+    </div>
+  )
+}
 
 export default Home
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,13 @@
 import Home from '../components/home/home'
 
-const ClothesPoem = () => <Home />
+const ClothesPoem = () => (
+  <Home
+    title="ShinyPoems"
+    subtitle="シャニマス衣装ポエム検索"
+    desc="シャイニーカラーズの衣装ポエムが検索できるサイトです"
+    url=""
+    ogpImg="./public/ogp.png"
+  />
+)
 
 export default ClothesPoem
