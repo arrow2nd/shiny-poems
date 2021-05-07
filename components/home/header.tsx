c63f5194e4c5df8c0878e551d473c9335f6a8c6b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-05-07 20:56:51 +0900

    fix: svg size

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="flex items-center justify-center w-full h-28 mb-2 bg-shiny">
       <a href="/">
-        <img className="h-14 md:h-16" src="/logo.svg" />
+        <img src="/logo.svg" width="280" />
       </a>
     </header>
   )
