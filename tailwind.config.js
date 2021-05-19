935b8a8c838345c4ee7ae494e3bebc6acdffda53
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-05-20 01:42:14 +0900

    add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
-  theme: {},
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px'
+    }
+  },
   variants: {
     extend: {}
   },
