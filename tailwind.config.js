e930de61577851e48cc14270128047977fc35348
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-05-20 01:52:44 +0900

    add: font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      body: ['"M PLUS Rounded 1c"']
+    },
     screens: {
       sm: '640px',
       md: '768px',
