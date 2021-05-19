df9654157b3fda6bd62baacf9d7ca7176572356b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-05-19 23:15:22 +0900

    add: M PLUS Rounded 1c

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import '@fontsource/m-plus-rounded-1c/400.css'
 import '../styles/globals.css'
 
 function App({ Component, pageProps }) {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,7 @@
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    fontFamily: {
-      body: [
-        'Avenir',
-        '"Helvetica Neue"',
-        'Helvetica',
-        'Arial',
-        '"Hiragino Sans"',
-        'ヒラギノ角ゴシック',
-        'YuGothic',
-        '"Yu Gothic"',
-        'メイリオ',
-        'Meiryo',
-        '"ＭＳ Ｐゴシック"',
-        'MS PGothic',
-        'monospace'
-      ]
-    }
-  },
+  theme: {},
   variants: {
     extend: {}
   },
