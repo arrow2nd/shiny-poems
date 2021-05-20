3aeca1112c02e4b1b7033c9eea093f750bcbc379
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-05-20 16:03:48 +0900

    update

---
+++ data/color-list.ts
@@ -0,0 +1,134 @@
+import { Color } from '../types/color'
+
+export const colorList: Color[] = [
+  {
+    idolName: '大崎甘奈',
+    className: 'bg-amana',
+    hex: 'F54275'
+  },
+  {
+    idolName: '芹沢あさひ',
+    className: 'bg-asahi',
+    hex: 'F30100'
+  },
+  {
+    idolName: '園田智代子',
+    className: 'bg-chiyoko',
+    hex: 'F93B90'
+  },
+  {
+    idolName: '桑山千雪',
+    className: 'bg-chiyuki',
+    hex: 'FBFAFA'
+  },
+  {
+    idolName: '黛冬優子',
+    className: 'bg-fuyuko',
+    hex: '5CE626'
+  },
+  {
+    idolName: '市川雛菜',
+    className: 'bg-hinana',
+    hex: 'FFC639'
+  },
+  {
+    idolName: '風野灯織',
+    className: 'bg-hiori',
+    hex: '144384'
+  },
+  {
+    idolName: '西城樹里',
+    className: 'bg-juri',
+    hex: 'FFC602'
+  },
+  {
+    idolName: '小宮果穂',
+    className: 'bg-kaho',
+    hex: 'E5461C'
+  },
+  {
+    idolName: '幽谷霧子',
+    className: 'bg-kiriko',
+    hex: 'D9F2FF'
+  },
+  {
+    idolName: '月岡恋鐘',
+    className: 'bg-kogane',
+    hex: 'F84CAD'
+  },
+  {
+    idolName: '福丸小糸',
+    className: 'bg-koito',
+    hex: '7967C3'
+  },
+  {
+    idolName: '斑鳩ルカ',
+    className: 'bg-luca',
+    hex: '1E140E'
+  },
+  {
+    idolName: '樋口円香',
+    className: 'bg-madoka',
+    hex: 'BE1E3E'
+  },
+  {
+    idolName: '田中摩美々',
+    className: 'bg-mamimi',
+    hex: 'A846FB'
+  },
+  {
+    idolName: '櫻木真乃',
+    className: 'bg-mano',
+    hex: 'FFBAD6'
+  },
+  {
+    idolName: '八宮めぐる',
+    className: 'bg-meguru',
+    hex: 'FFE012'
+  },
+  {
+    idolName: '和泉愛依',
+    className: 'bg-mei',
+    hex: 'FF00FF'
+  },
+  {
+    idolName: '緋田美琴',
+    className: 'bg-mikoto',
+    hex: '760E10'
+  },
+  {
+    idolName: '有栖川夏葉',
+    className: 'bg-natsuha',
+    hex: '90E667'
+  },
+  {
+    idolName: '七草にちか',
+    className: 'bg-nichika',
+    hex: 'A5CFB6'
+  },
+  {
+    idolName: '杜野凛世',
+    className: 'bg-rinze',
+    hex: '89C3EB'
+  },
+  {
+    idolName: '白瀬咲耶',
+    className: 'bg-sakuya',
+    hex: '006047'
+  },
+  {
+    idolName: '大崎甜花',
+    className: 'bg-tenka',
+    hex: 'E75BEC'
+  },
+  {
+    idolName: '浅倉透',
+    className: 'bg-toru',
+    hex: '50D0D0'
+  },
+  {
+    idolName: '三峰結華',
+    className: 'bg-yuika',
+    hex: '3B91C4'
+  }
+]
