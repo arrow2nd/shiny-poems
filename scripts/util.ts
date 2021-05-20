dfc1c4013c26a1b42e5f411307dd8016107a7f00
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-05-20 16:05:03 +0900

    change: OGP image

--- scripts/util.ts
+++ scripts/util.ts
@@ -1,3 +1,9 @@
+/**
+ * 文の区切りで分割
+ *
+ * @param text 文字列
+ * @returns 文字列配列
+ */
 export const splitText = (text: string) => {
   const splited = text.match(/(.*?[。！？!?])(.*)/)
 
@@ -7,4 +13,14 @@ export const splitText = (text: string) => {
   }
 
   return [splited[1], splited[2]]
+}
+
+/**
+ * 文字列をエンコード
+ *
+ * @param str 文字列
+ * @returns エンコードされた文字列
+ */
+export const encode = (str: string) => {
+  return encodeURIComponent(str.replace(/\,/g, '%2C').replace(/\//g, '%2F'))
 }
