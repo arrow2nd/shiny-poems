9e79b3d50a25603166f7d2f1e0427c5a0ee3e5ee
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-08-30 23:46:49 +0900

    add: Show the poem specified in the id

--- components/ui/index.tsx
+++ components/ui/index.tsx
@@ -1,10 +1,15 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 import { searchPoem } from '../../scripts/search-poem'
 import { Poem } from '../../types/poem'
 import Poems from '../poems'
 import Search from './search'
 
-const UI = () => {
+type Props = {
+  poemText: string
+}
+
+const UI = ({ poemText }: Props) => {
   const [searchResults, setSearchResults] = useState([] as Poem[])
 
   // 条件とキーワードから検索する
@@ -12,6 +17,13 @@ const UI = () => {
     const newSearchResults = searchPoem(type, keyword)
     setSearchResults(newSearchResults)
   }
+
+  // idで指定されたポエムがあれば検索する
+  useEffect(() => {
+    if (poemText != '') {
+      handleSearch('text', poemText)
+    }
+  }, [])
 
   return (
     <div className="flex-grow mx-4">
--- scripts/generate-ogp.ts
+++ scripts/generate-ogp.ts
@@ -4,24 +4,25 @@ import { poemList } from '../data/poem-list'
 import { colorList } from '../data/color-list'
 import cloudinary from 'cloudinary'
 
-export const generateOgpImageUrl = (query: ParsedUrlQuery): string => {
+export const generateOgpImageUrl = (
+  query: ParsedUrlQuery
+): [string, string] => {
   const defaultOgp = 'https://shiny-poems.vercel.app/ogp-home.png'
 
   // クエリからidを取得
   const id = query.id || ''
   const idStr = Array.isArray(id) ? '' : id
-  if (!idStr) return defaultOgp
+  if (!idStr) return [defaultOgp, '']
 
   // idからポエムを取得
   const poem = poemList.find((e) => e.id === idStr)
-  if (!poem) return defaultOgp
+  if (!poem) return [defaultOgp, '']
 
   // アイドル名からイメージカラーを取得
   const idolColor = colorList.find((e) => e.idolName === poem.idolName)
-
   const poemText = splitText(poem.text).join('\n')
 
-  return cloudinary.v2.url('shiny-poems/ogp-base.png', {
+  const ogpImgUrl = cloudinary.v2.url('shiny-poems/ogp-base.png', {
     secure: true,
     sign_url: true,
     transformation: [
@@ -38,4 +39,6 @@ export const generateOgpImageUrl = (query: ParsedUrlQuery): string => {
       }
     ]
   })
+
+  return [ogpImgUrl, poem.text]
 }
