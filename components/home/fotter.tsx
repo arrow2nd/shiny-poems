44dc493371a5d9dc736de9d850b28ea68625ce3d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-09-12 16:46:01 +0900

    fix

--- components/home/fotter.tsx
+++ components/home/fotter.tsx
@@ -1,46 +1,26 @@
-import { FaGithubAlt, FaTwitter } from 'react-icons/fa'
-import LinkButton from './link-button'
+import AccountLinks from './account-links'
+import Link from '../common/link'
 
 const Footer = () => (
   <footer className="flex flex-col items-center justify-center text-center w-full h-80 md:h-64 px-5 bg-shiny text-white">
-    <div className="flex flex-row mb-8">
-      <LinkButton href="https://twitter.com/arrow_2nd">
-        <FaTwitter />
-      </LinkButton>
-      <LinkButton href="https://github.com/arrow2nd/shiny-poems">
-        <FaGithubAlt />
-      </LinkButton>
-    </div>
+    <AccountLinks />
     <div className="mb-2">
-      <span>
-        {'Logo icon made by '}
-        <a
-          className="underline"
-          title="Pixel perfect"
-          href="https://www.flaticon.com/authors/pixel-perfect"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Pixel perfect
-        </a>
-        {' from '}
-        <a
-          className="underline"
-          title="Flaticon"
-          href="https://www.flaticon.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          www.flaticon.com
-        </a>
-      </span>
+      {'All data is obtained from '}
+      <Link text="imasp@rql" href="https://sparql.crssnky.xyz/imas/" />
     </div>
-    <div>
-      <p>
-        The copyright of all content related to THE IDOLM@STER belongs to BANDAI
-        NAMCO Entertainment Inc.
-      </p>
-    </div>
+    <p className="mb-2">
+      {'Logo icon made by '}
+      <Link
+        text="Pixel perfect"
+        href="https://www.flaticon.com/authors/pixel-perfect"
+      />
+      {' from '}
+      <Link text="flaticon" href="https://www.flaticon.com/" />
+    </p>
+    <p>
+      The copyright of all content related to THE IDOLM@STER belongs to BANDAI
+      NAMCO Entertainment Inc.
+    </p>
   </footer>
 )
 
