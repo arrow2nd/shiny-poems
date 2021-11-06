1129bd2899dacd048bfcd98bc7f4a3cefe9da766
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-11-06 23:01:58 +0900

    add: title

--- components/home/account-links.tsx
+++ components/home/account-links.tsx
@@ -3,10 +3,10 @@ import LinkButton from '../common/link/button'
 
 const AccountLinks = () => (
   <div className="flex flex-row mb-6">
-    <LinkButton href="https://twitter.com/arrow_2nd">
+    <LinkButton title="Twitter" href="https://twitter.com/arrow_2nd">
       <FaTwitter />
     </LinkButton>
-    <LinkButton href="https://github.com/arrow2nd/shiny-poems">
+    <LinkButton title="GitHub" href="https://github.com/arrow2nd/shiny-poems">
       <FaGithubAlt />
     </LinkButton>
   </div>
