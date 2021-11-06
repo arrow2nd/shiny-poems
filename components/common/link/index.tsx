91084edbcf764d29f89b95a764bf5fb47bee749e
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-11-06 21:54:28 +0900

    change: layout

--- components/common/link/index.tsx
+++ components/common/link/index.tsx
@@ -5,7 +5,7 @@ type Props = {
 
 const Link = ({ text, href }: Props) => (
   <a
-    className="underline"
+    className="underline text-white hover:text-gray-500 transition-colors"
     title={text}
     href={href}
     target="_blank"
--- components/home/account-links.tsx
+++ components/home/account-links.tsx
@@ -2,7 +2,7 @@ import { FaGithubAlt, FaTwitter } from 'react-icons/fa'
 import LinkButton from '../common/link/button'
 
 const AccountLinks = () => (
-  <div className="flex flex-row mb-8">
+  <div className="flex flex-row mb-6">
     <LinkButton href="https://twitter.com/arrow_2nd">
       <FaTwitter />
     </LinkButton>
