6eb989efc384f1f36f9a2f514457ac835038d811
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-12-11 20:24:43 +0900

    fix: 高さ

--- components/home/header/index.tsx
+++ components/home/header/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
 const Header = () => (
-  <header className="flex justify-center items-center w-full h-24 mb-16 bg-shiny">
+  <header className="flex justify-center items-center w-full h-24 mb-12 md:mb-16 bg-shiny">
     <a className="text-none" href="">
       <Image src="/logo.svg" alt="logo" width={210} height={47} />
     </a>
