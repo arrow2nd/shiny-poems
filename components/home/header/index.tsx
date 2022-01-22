b0d70722a214ee9864d1b71eba500bfa74f653eb
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-01-22 17:07:31 +0900

    change: ロゴのサイズ

--- components/home/header/index.tsx
+++ components/home/header/index.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image'
+import LogoImg from 'public/logo.png'
 
 const Header = () => (
   <header className="flex justify-center items-center w-full h-24 mb-12 md:mb-16 bg-shiny">
     <a className="text-none" href="">
-      <Image src="/logo.svg" alt="logo" width={210} height={47} />
+      <Image
+        src={LogoImg}
+        alt="logo"
+        width={256}
+        height={58}
+        placeholder="blur"
+      />
     </a>
   </header>
 )
