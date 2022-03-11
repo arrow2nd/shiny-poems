3ae1f05a810b151b5c49397d0681261010a11945
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-03-11 20:46:54 +0900

    test: e2eテスト時に画面録画を撮るよう変更

--- e2e/shiny-poems.spec.ts
+++ e2e/shiny-poems.spec.ts
@@ -1,13 +1,14 @@
 import { expect, test } from '@playwright/test'
 
-test('検索してスクリーンショットを撮影', async ({ page }, testInfo) => {
-  await page.goto('http://localhost:3000')
-
-  // 検索前のスクリーンショット
-  await page.screenshot({
-    path: `screenshots/before-${testInfo.project.name}.png`,
-    fullPage: true
+test('検索して画面を記録', async ({ contextOptions, browser }, { project }) => {
+  const saveDir = `e2e/results/${project.name}`
+  const context = await browser.newContext({
+    ...contextOptions,
+    recordVideo: { dir: saveDir }
   })
+
+  const page = await context.newPage()
+  await page.goto('http://localhost:3000')
 
   // テキストボックスに入力
   await page.click('[data-testid="poem-search-textbox"]')
@@ -17,15 +18,13 @@ test('検索してスクリーンショットを撮影', async ({ page }, testIn
 
   // 検索実行
   await page.click('[data-testid="poem-search-submit"]')
-
   await expect(page.locator('[data-testid="poem-card-text"] > p')).toHaveText([
     'ルームウェア。',
     'すまじきものは恋'
   ])
 
-  // 検索結果画面のスクリーンショット
-  await page.screenshot({
-    path: `screenshots/after-${testInfo.project.name}.png`,
-    fullPage: true
-  })
+  // ページ全体のスクリーンショット
+  await page.screenshot({ path: `${saveDir}/full-page.png`, fullPage: true })
+
+  await context.close()
 })
