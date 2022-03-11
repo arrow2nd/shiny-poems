69f4024243fe5bebf9b45a52d86150499b301ad1
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-03-11 10:54:12 +0900

    feat: 値が見つからなかった時のメッセージを追加

--- components/ui/search/select/index.tsx
+++ components/ui/search/select/index.tsx
@@ -57,6 +57,7 @@ const Select = (props: Props, ref: React.MutableRefObject<any>) => {
       options={options}
       styles={styles}
       theme={theme}
+      noOptionsMessage={() => 'みつかりません…'}
       onChange={handleChangeSelect}
       ref={ref}
     />
