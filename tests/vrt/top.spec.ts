6523cf3003019aa5d015c1eb776dc9111c8ee2d1
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-04-22 22:43:07 +0900

    ✅ e2eとvrtを分離

---
+++ tests/vrt/top.spec.ts
@@ -0,0 +1,38 @@
+import { Locator, Page, expect, test } from "@playwright/test";
+
+import {
+  baseUrl,
+  testSearchFromClotheName,
+  testSearchPoemFromText
+} from "../utils";
+
+// ファイル内のテストを並列に実行
+test.describe.configure({ mode: "parallel" });
+
+function getMaskLocations(page: Page): Locator[] {
+  const wave = page.locator("footer > div > svg");
+  const lastUpdated = page.getByTestId("last-updated");
+  return [wave, lastUpdated];
+}
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(baseUrl);
+});
+
+test("検索結果がひとつの場合", async ({ page }) => {
+  await testSearchPoemFromText(page);
+
+  await expect(page).toHaveScreenshot("search-result-single.png", {
+    fullPage: true,
+    mask: getMaskLocations(page)
+  });
+});
+
+test("検索結果が複数の場合", async ({ page }) => {
+  await testSearchFromClotheName(page);
+
+  await expect(page).toHaveScreenshot("search-result-multi.png", {
+    fullPage: true,
+    mask: getMaskLocations(page)
+  });
+});
