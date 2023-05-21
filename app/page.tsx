31be8dfea5407bd2ad27c8e6a49473be40d3abf0
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-05-21 17:07:01 +0900

    👍 metadataBaseを追加

--- app/page.tsx
+++ app/page.tsx
@@ -29,17 +29,18 @@ export async function generateMetadata({
   return {
     title,
     description,
+    metadataBase: new URL(url),
     openGraph: {
-      title: title,
+      title,
       description,
-      url: url,
+      url,
       siteName: title,
       type: "website",
       images: [{ url: imageUrl }]
     },
     twitter: {
       card: "summary_large_image",
-      title: title,
+      title,
       description,
       images: [{ url: imageUrl }]
     }
