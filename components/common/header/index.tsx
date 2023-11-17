7c7df00fd55bf33481d96a90d483c609b22f16e6
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-11-17 23:22:54 +0900

    👍 ロゴクリックでハードリロードさせるように

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import Link from "next/link";
 import LogoImg from "public/logo.png";
 
 import GitHubCorner from "./github-corner";
 
 const Header = () => (
   <header className="flex justify-center items-center py-20 w-full">
-    <Link className="w-64 md:w-80 text-none" href="/" data-testid="logo">
+    <a className="w-64 md:w-80 text-none" href="/" data-testid="logo">
       <Image
         src={LogoImg}
         alt="logo"
@@ -15,7 +14,7 @@ const Header = () => (
         placeholder="empty"
         priority
       />
-    </Link>
+    </a>
     <GitHubCorner
       href="https://github.com/arrow2nd/shiny-poems"
       bannerColor="#4C7ABE"
