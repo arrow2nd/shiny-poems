a26fca3a0f252d847cb7824f0862a10cec06a115
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-11-19 00:12:12 +0900

    💚 見た目のテストを削除、ロジックのテストを追加

--- components/poems/card/index.test.tsx
+++ components/poems/card/index.test.tsx
@@ -4,61 +4,48 @@ import { Poem } from "types/poem";
 
 import Card from "./index";
 
-const testCases: { title: string; poem: Poem }[] = [
-  {
-    title: "改行されるパターン (句点)",
-    poem: {
-      id: "ShinySummer_SakuragiMano",
-      idolName: "櫻木真乃",
-      clothesTitle: "シャイニーサマー",
-      clothesName: "シャイニーサマー",
-      text: "1stシーズンの水着。めぐるちゃんとお揃い柄♪"
-    }
-  },
-  {
-    title: "1つ目の句点で改行されるパターン",
-    poem: {
-      id: "One_Day_Officer",
-      idolName: "櫻木真乃",
-      clothesTitle: "ワンデイシリーズ",
-      clothesName: "ワンデイオフィサー",
-      text: "勤務回想録。敬礼。みなさんの元へ急行しますっ"
-    }
-  },
-  {
-    title: "改行されるパターン (スペース)",
-    poem: {
-      id: "DressUpParfum_SakuragiMano",
-      idolName: "櫻木真乃",
-      clothesTitle: "ドレスアップパルファム",
-      clothesName: "ドレスアップパルファム",
-      text: "Cheers! 泡沫の間の輝き"
-    }
-  },
-  {
-    title: "改行されないパターン (読点)",
-    poem: {
-      id: "BaitwudeiRestaurant_SakuragiMano",
-      idolName: "櫻木真乃",
-      clothesTitle: "バイトゥデイレストラント",
-      clothesName: "バイトゥデイレストラント",
-      text: "いらっしゃいませ、輝ける場所へ"
-    }
-  },
-  {
-    title: "改行されないパターン (スペースなし)",
-    poem: {
-      id: "FashionableSummer_SakuragiMano",
-      idolName: "櫻木真乃",
-      clothesTitle: "ファッショナブルサマー",
-      clothesName: "ファッショナブルサマー",
-      text: "サマーダイブ！とうめいな空と海で遊ぶ"
-    }
-  }
-];
+const poem: Poem = {
+  id: "HottestSummer_AketaMikoto",
+  idolName: "緋田美琴",
+  clothesTitle: "ホッテストサマー",
+  clothesName: "ホッテストサマー",
+  text: "マリンノート＠drown"
+};
 
-test.each(testCases)("$title", ({ poem }) => {
-  const { container } = render(<Card poem={poem} />);
-  expect(container).toMatchSnapshot();
+describe("Card", () => {
+  test("内容が表示されている", () => {
+    const { getByTestId } = render(<Card poem={poem} />);
+
+    expect(getByTestId("poem-card-text")).toHaveTextContent(poem.text);
+    expect(getByTestId("poem-card-clothe")).toHaveTextContent(poem.clothesName);
+    expect(getByTestId("poem-card-idol")).toHaveTextContent(poem.idolName);
+  });
+
+  test("個人カラーが見付からない場合ブランドカラーが使われる", () => {
+    const { getByTestId } = render(
+      <Card
+        poem={{
+          id: "test",
+          idolName: "test",
+          clothesTitle: "test",
+          clothesName: "test",
+          text: "test"
+        }}
+      />
+    );
+
+    expect(getByTestId("poem-card-accent")).toHaveStyle(
+      "background-color: rgb(120, 174, 255);"
+    );
+  });
+
+  test("共有リンクの内容が正しい", () => {
+    const { getByRole } = render(<Card poem={poem} />);
+    const href = getByRole("link").getAttribute("href");
+
+    expect(href).toContain(encodeURIComponent("#シャニマス"));
+    expect(href).toContain(encodeURIComponent(`#${poem.clothesName}`));
+    expect(href).toContain(encodeURIComponent(`#${poem.idolName}`));
+    expect(href).toContain(encodeURIComponent(`?id=${poem.id}`));
+  });
 });
-
