d77f55a2a9c3bc0763cfef38d52605491ba7be59
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-11-18 23:49:16 +0900

    💚 VRTを追加・修正

---
+++ tests/vrt/poem-card.spec.ts
@@ -0,0 +1,73 @@
+import { Poem } from "types/poem";
+
+import { expect, test } from "../fixtures";
+import { getWaveMask } from "./mask";
+
+const poems: Poem[] = [
+  // 句点で改行
+  {
+    id: "ShinySummer_SakuragiMano",
+    idolName: "櫻木真乃",
+    clothesTitle: "シャイニーサマー",
+    clothesName: "シャイニーサマー",
+    text: "1stシーズンの水着。めぐるちゃんとお揃い柄♪"
+  },
+  // 最初の句点で改行
+  {
+    id: "One_Day_Officer",
+    idolName: "櫻木真乃",
+    clothesTitle: "ワンデイシリーズ",
+    clothesName: "ワンデイオフィサー",
+    text: "勤務回想録。敬礼。みなさんの元へ急行しますっ"
+  },
+  // "!"で改行
+  {
+    id: "DressUpParfum_SakuragiMano",
+    idolName: "櫻木真乃",
+    clothesTitle: "ドレスアップパルファム",
+    clothesName: "ドレスアップパルファム",
+    text: "Cheers! 泡沫の間の輝き"
+  },
+  // "！"で改行
+  {
+    id: "FashionableSummer_SakuragiMano",
+    idolName: "櫻木真乃",
+    clothesTitle: "ファッショナブルサマー",
+    clothesName: "ファッショナブルサマー",
+    text: "サマーダイブ！とうめいな空と海で遊ぶ"
+  },
+  // 改行なし
+  {
+    id: "BaitwudeiRestaurant_SakuragiMano",
+    idolName: "櫻木真乃",
+    clothesTitle: "バイトゥデイレストラント",
+    clothesName: "バイトゥデイレストラント",
+    text: "いらっしゃいませ、輝ける場所へ"
+  }
+];
+
+test("カードのレイアウトが変化していない", async ({ shinyPoems }) => {
+  const { page } = shinyPoems;
+
+  // 検索結果を上書き
+  await page.route("/", async (route, req) => {
+    if (req.method() !== "POST") {
+      return route.continue();
+    }
+
+    const response = await route.fetch();
+    const body = `0:["$@1",["development",null]]
+1:{"poems":${JSON.stringify(poems)}}
+`;
+
+    return route.fulfill({ response, body });
+  });
+
+  await page.goto("/");
+  await shinyPoems.searchByQuery("test");
+
+  await expect(page).toHaveScreenshot({
+    fullPage: true,
+    mask: getWaveMask(page)
+  });
+});
---
+++ tests/vrt/search-results.spec.ts
@@ -0,0 +1,35 @@
+import { expect, test } from "../fixtures";
+import { getWaveMask } from "./mask"
+
+test.beforeEach(async ({ shinyPoems: { page } }) => {
+  await page.goto("/");
+});
+
+test("検索結果なし", async ({ shinyPoems }) => {
+  const { page } = shinyPoems;
+
+  await expect(page).toHaveScreenshot({
+    fullPage: true,
+    mask: getWaveMask(page)
+  });
+});
+
+test("検索結果がひとつ", async ({ shinyPoems }) => {
+  const { page } = shinyPoems;
+  await shinyPoems.searchByQuery("Shiny いくつもの可能性を繋ぎ照らす");
+
+  await expect(page).toHaveScreenshot({
+    fullPage: true,
+    mask: getWaveMask(page)
+  });
+});
+
+test("検索結果が複数", async ({ shinyPoems }) => {
+  const { page } = shinyPoems;
+  await shinyPoems.searchByClothe("ほしあかり");
+
+  await expect(page).toHaveScreenshot({
+    fullPage: true,
+    mask: getWaveMask(page)
+  });
+});
