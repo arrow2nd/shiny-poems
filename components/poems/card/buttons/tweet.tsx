1b62c9a7c5469bc91d3d5ebb484396c0ee97924c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-12-28 22:51:33 +0900

    👍 Twitterシェアをしぇあ.comに置き換え

--- components/poems/card/buttons/tweet.tsx
+++ components/poems/card/buttons/tweet.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FiTwitter } from "react-icons/fi";
+import { FiShare2 } from "react-icons/fi";
 
 type Props = {
   text: string;
@@ -8,17 +8,17 @@ type Props = {
 
 const TweetButton = ({ text }: Props) => {
   const encodedText = encodeURIComponent(text);
-  const tweetUrl = `https://twitter.com/intent/tweet?text=${encodedText}`;
+  const tweetUrl = `https://しぇあ.com?text=${encodedText}`;
 
   return (
     <a
       className="text-xl transition-colors hover:text-black"
-      title="ツイートする"
+      title="シェアする"
       href={tweetUrl}
       target="_blank"
       rel="noopener noreferrer"
     >
-      <FiTwitter />
+      <FiShare2 />
     </a>
   );
 };
