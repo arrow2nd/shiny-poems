269625d6cecf40a9d8f21a9d59426d3491613eaa
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-03-21 21:45:25 +0900

    👍 Vercel Analytics -> Cloudflare Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 import "styles/globals.css";
 
@@ -12,7 +11,11 @@ export default function RootLayout({
       <body className="flex min-h-screen flex-col bg-neutral-white">
         {children}
       </body>
-      <Analytics />
+      <script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "05635d43b87d4e82a58cbbcb8ff3b1b5"}'
+      ></script>
     </html>
   );
 }
