6d1c2a814538cfc46ec145a2e46b3bda0b07888b
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-03-23 22:15:20 +0900

    🚑 scriptをheadの中に移動

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        <script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token": "05635d43b87d4e82a58cbbcb8ff3b1b5"}'
+        ></script>
+      </head>
       <body className="flex min-h-screen flex-col bg-neutral-white">
         {children}
       </body>
-      <script
-        defer
-        src="https://static.cloudflareinsights.com/beacon.min.js"
-        data-cf-beacon='{"token": "05635d43b87d4e82a58cbbcb8ff3b1b5"}'
-      ></script>
     </html>
   );
 }
